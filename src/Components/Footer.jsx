1b8848685444777e0f2739225729c9b73a4a0689
rvd003 <101614481+rvd003@users.noreply.github.com>
2022-07-20 23:31:40 +0530

    day2

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import "./Navbar.css"
+const Footer = () => {
+  return (
+    <div className='footer'> 
+        <div className="Fo"><h4>Applications</h4>
+        <img  className="fimg" src="https://tse3.mm.bing.net/th?id=OIP.sEFSb4FiXX2nAZWMmw6FNAHaHa&pid=Api&P=0"/><br/> 
+        <img className="fimg" src="https://tse4.mm.bing.net/th?id=OIP.MS6B-WWW-zcySwGIymZuzwHaIT&pid=Api&P=0"/><br/>  
+        <img className="fimg" src="https://tse4.mm.bing.net/th?id=OIP.7tk2xUK0IZgKOY3r_vIrRgHaHa&pid=Api&P=0"/> </div>
+        <div className="Fo"><h4>Browser Extension</h4>
+        <img  className="fimg1" src="https://tse3.mm.bing.net/th?id=OIP.QlPsPJNPpX1enNrbqNkbuQHaHa&pid=Api&P=0"/><br/> 
+        <img className="fimg1" src="https://tse4.mm.bing.net/th?id=OIP.PK5Sx0VG8gf-ajYuxtQmowHaHO&pid=Api&P=0"/><br/>  
+        <img className="fimg1" src="https://tse2.mm.bing.net/th?id=OIP.rg3AMUFEXf36sb6lRqwLXgHaHa&pid=Api&P=0"/><br/>
+        <img className="fimg1" src="https://tse4.mm.bing.net/th?id=OIP.hutY4Y5cLhhfmzd13Iw6pgHaHa&pid=Api&P=0"/></div>
+        <div className="Fo"><h4>Resource</h4>
+        <p><u>Solution</u></p>
+        <p><u>Integration</u></p>
+        <p><u>Help</u></p>
+        <p><u>Time Tracker</u></p>
+        <p><u>Blog</u></p>
+        </div>
+        <div className="Fo"><h4>About</h4>
+        <p><u>About Tmetric</u></p>
+        <p><u>Terme And Service</u></p>
+        <p><u>Privacy Policy</u></p>
+        <p><u>Cookie Policy</u></p>
+        </div> 
+    </div>
+    
+    
+    
+  )
+}
+
+export default Footer
